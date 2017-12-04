324e10205410be141451572b20d7115a150669c7
Jamie Catania <catania.jamie@gmail.com>
2017-12-04 16:52:04 -0500

    Initial commit

---
+++ assets/js/chart-themes/chart-theme-1.js
@@ -0,0 +1,12 @@
+//Theme-1
+$(document).ready(function() {
+    
+    $('.chart').easyPieChart({		
+		barColor:'#00BCD4',
+		trackColor: '#e8e8e8',
+		scaleColor: false,
+		lineWidth : 5,
+		animate: 2000
+	});    
+
+});
