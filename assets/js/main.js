2880fd6882935103fa7a5daac6760abf78b2057c
Unknown <catania.jamie@gmail.com>
2018-01-02 15:47:30 -0500

    Set intro banner height to window height on load

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,9 +108,13 @@ $(document).ready(function () {
         }
     });
 
+    // $(window).on('load', function () {
+    // if ($('body').hasClass('mobile')) {
+    //     $('#intro').css({ height: window.innerHeight });
+    //   }
+    // });
+
     $(window).on('load', function () {
-    if ($('body').hasClass('mobile')) {
-        $('#intro').css({ height: window.innerHeight });
-      }
+        $('.full-frame').css({ height: $(window).height() });
     });
 });
